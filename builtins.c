2e01aa80baa6308f1589c88f39e0d77d6442fadd
legendarysandbird <legendarysandbird@gmail.com>
2022-02-03 11:11:25 -0800

    Fix String:plus

--- builtins.c
+++ builtins.c
@@ -324,20 +324,15 @@ obj_ref native_String_plus() {
 	obj_ref other = (vm_fp - 1)->obj;
 	assert_is_type(other, the_class_String);
 	obj_String other_str = (obj_String) other;
-	char* new_text = (char*)malloc(sizeof(this_str->text) + sizeof(other_str->text));
+	char* new_text = (char*)malloc(strlen(this_str->text) + strlen(other_str->text));
 	strcpy(new_text, this_str->text);
-	int offset = sizeof(this_str->text) / sizeof(char*);
-	strcpy(new_text + offset, other_str->text);
+	strcat(new_text, other_str->text);
 	obj_ref created_string = new_string(new_text);
 	return created_string;
 }
 
 vm_Word method_String_plus[] = {
 	{.instr = vm_op_enter},
-	{.instr = vm_op_load},
-	{.intval = 0},
-	{.instr = vm_op_load},
-	{.intval = -1},
 	{.instr = vm_op_call_native},
 	{.native = native_String_plus},
 	{.instr = vm_op_return},
@@ -382,8 +377,8 @@ struct  class_struct  the_class_String_struct = {
         method_String_string,
         method_String_print,
         method_String_equals,
-		method_String_plus,
-		method_String_less
+		method_String_less,
+		method_String_plus
 };
 
 class_ref the_class_String = &the_class_String_struct;
